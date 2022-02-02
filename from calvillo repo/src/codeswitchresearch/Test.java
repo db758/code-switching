17ee905010b89a220645fc1d7341555bc723806e
db758 <35625183+db758@users.noreply.github.com>
2022-02-02 09:39:42 -0500

    organising files from calvillo repo
    data folder located on local (too big for git)

---
+++ from calvillo repo/src/codeswitchresearch/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codeswitchresearch;
+
+/**
+ *
+ * @author Le
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //double d = Double.NEGATIVE_INFINITY;
+        double a = -5.0;
+        double surprisal = -Math.log10(a);
+        System.out.println("surprisal of a: " + surprisal);
+        
+    }
+}
